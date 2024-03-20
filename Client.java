8358188ee0716fc4d5796a8569cda5e4e7ef59c4
HoverfishOTS <rwei11254@gmail.com>
2024-03-20 14:04:38 -0700

    wrote a bit of client object, more info needed on how other things are implemented

--- Client.java
+++ Client.java
@@ -1,3 +1,22 @@
 public class Client {
     // client object
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private int age;
+
+    private Produce produce[];
+    private int produceCount;
+
+    // name, age, and at least one contact method are required, null will be passed in for optional fields
+    public Client(String name, String address, String phone, String email, int age) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.age = age;
+    }
+
+    }
 }
