28728ac11dce7e4bffe764d60d63ac1d3df85038
Medha <116462079+MedhaS1@users.noreply.github.com>
2024-03-24 21:49:21 -0700

    Made a plan for FarmerUI

--- FarmerUI.java
+++ FarmerUI.java
@@ -1,3 +1,23 @@
 public class FarmerUI {
     // Farmer User Interface (terminal prompts)
+    // handle user input/output through terminal
+    // present a list of options
+    // have the farmer input numerical or alphabetical keypresses correspondingly
+    
+
+
+
+    // take in input here and add to other classes?
+    // add to BagInterface?
+
+    // produce #s: kale, beets, fennel, arugula, swiss chard, broccoli, dill, cilantro, collard greens, bok choy, mizuna, green onions, mustard greens, lettuce, black berries, joy choi, eggplants, hot peppers, carrots, and cauliflower
+    // change recommended price per item
+    // Inventory object will have stuff
+
+    // # of customers coming on that day?
+
+    // take in new clients and add to client base
+
+
+
 }
