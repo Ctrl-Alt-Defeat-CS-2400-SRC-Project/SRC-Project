ad8bdb54190753899d98943ce2b226cd9cb0223c
HoverfishOTS <rwei11254@gmail.com>
2024-04-15 16:19:57 -0700

    updated Client, added constructors for more cases

--- Client.java
+++ Client.java
@@ -1,49 +1,135 @@
+/**
+ * Class that implements a client object, which is used to store information about a person in need of assistance.
+ * @author Ryan Wei
+ */
+
 public class Client {
-    // client object
-    private String name;
+    private String userName; //user name for anonymity, as those who are in need may not want to share their real name
     private String address;
     private String phone;
     private String email;
     private int age;
 
-    // name, age, and at least one contact method are required, null will be passed in for optional fields
+    /**
+     * Constructor for the client object
+     * 
+     * @param name the user name of the client, is required
+     * @param age the age of the client, is required
+     * @param address the address of the client, is optional
+     * @param phone the phone number of the client, either phone or email is required
+     * @param email the email of the client, either phone or email is required
+     *  */ 
     public Client(String name, String address, String phone, String email, int age) {
-        this.name = name;
+        this.userName = name;
         this.address = address;
         this.phone = phone;
         this.email = email;
         this.age = age;
     }
 
-    public Client(String name, String address, String phone, int age) {
+    /**
+     * Alternate constructor that does not require an address
+     * 
+     * @param name the user name of the client, is required
+     * @param age the age of the client, is required
+     * @param phone the phone number of the client, either phone or email is required
+     * @param email the email of the client, either phone or email is required
+     *  */
+    public Client(String name, String phone, int age, String email) {
+        this(name, null, phone, email, age);
+    }
+
+    /**
+     * Alternate constructor that does not require an phone
+     * 
+     * @param name the user name of the client, is required
+     * @param age the age of the client, is required
+     * @param address the address of the client, is optional
+     * @param email the phone number of the client, either phone or email is required
+     *  */
+    public Client(String name, String address, String email, int age) {
+        this(name, address, null, email, age);
+    }
+
+    /**
+     * Alternate constructor that does not require an email
+     * 
+     * @param name the user name of the client, is required
+     * @param age the age of the client, is required
+     * @param address the address of the client, is optional
+     * @param phone the phone number of the client, either phone or email is required
+     *  */
+    public Client(String name, int age, String address, String phone) {
         this(name, address, phone, null, age);
     }
 
+    /**
+     * Alternate constructor that does not require an address or email
+     * 
+     * @param name the user name of the client, is required
+     * @param age the age of the client, is required
+     * @param phone the phone number of the client, either phone or email is required
+     *  */
     public Client(String name, String phone, int age) {
         this(name, null, phone, null, age);
     }
 
-    public String getName() {
-        return name;
+    /**
+     * Alternate constructor that does not require an address or phone number
+     * 
+     * @param name the user name of the client, is required
+     * @param age the age of the client, is required
+     * @param email the email of the client, either phone or email is required
+     *  */
+    public Client(String name, int age, String email) {
+        this(name, null, null, email, age);
     }
 
+    /**
+     * retrieves the user name of the client
+     * @return userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * retrieves the address of the client
+     * @return address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * retrieves the phone number of the client
+     * @return phone
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * retrieves the email of the client
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * retrieves the age of the client
+     * @return age
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * retrieves all the information of the client
+     * @return the information of the client
+     */
     public String toString() {
-        return "Name: " + name + "\nAddress: " + address + "\nPhone: " + phone + "\nEmail: " + email + "\nAge: " + age;
+        return "Name: " + userName + "\nAddress: " + address + "\nPhone: " + phone + "\nEmail: " + email + "\nAge: " + age;
     }
 }
