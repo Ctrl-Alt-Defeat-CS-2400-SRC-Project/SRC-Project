3be34ef06a347e2eca5c995915b56ecefed9b968
HoverfishOTS <rwei11254@gmail.com>
2024-04-28 17:45:23 -0700

    Added option of choosing between farmer and client ui

--- Driver.java
+++ Driver.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * a main class to demo code
+ * 
+ * @author Ryan Wei
+ */
 public class Driver {
     public static void main(String[] args) {
-        //FarmerUI.start();
-        ClientUI.start();
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Welcome! Please select your role:");
+        System.out.println("1. Farmer");
+        System.out.println("2. Client");
+        int role = kb.nextInt();
+        kb.nextLine();
+        if (role == 1) {
+            FarmerUI.start();
+        } else if (role == 2) {
+            ClientUI.start();
+        } else {
+            System.out.println("Invalid input. Please try again. Exiting...");
+        }
+        kb.close();
     }
 }
